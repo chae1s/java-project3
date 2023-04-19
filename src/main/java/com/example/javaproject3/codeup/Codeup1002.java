70d6db9448ba5463326fae95652f91b55ad118c7
chae1s <chae1zd.b@gmail.com>
2023-04-19 16:43:16 +0900

    feat: Codeup 문제풀이  - codeup 1001번문제 풀이  - codeup 1002번문제 풀이  - codeup 1003번문제 풀이  - codeup 1004번문제 풀이  - codeup 1005번문제 풀이

--- src/main/java/com/example/javaproject3/codeup/Codeup1002.java
+++ src/main/java/com/example/javaproject3/codeup/Codeup1002.java
@@ -1,7 +1,7 @@
 package com.example.javaproject3.codeup;
 
-public class Codeup1001 {
+public class Codeup1002 {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello World");
     }
 }
--- src/main/java/com/example/javaproject3/codeup/Codeup1005.java
+++ src/main/java/com/example/javaproject3/codeup/Codeup1005.java
@@ -1,7 +1,7 @@
 package com.example.javaproject3.codeup;
 
-public class Codeup1001 {
+public class Codeup1005 {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("\"Hello World\"");
     }
 }
