c13e835756c9200d775b2fe6f39ca441846870ca
chae1s <chae1zd.b@gmail.com>
2023-04-20 11:22:32 +0900

    feat: PrintfEx 추가  - printf 실습

---
+++ src/main/java/com/example/javaproject3/week1/PrintWhy.java
@@ -0,0 +1,12 @@
+package com.example.javaproject3.week1;
+
+public class PrintWhy {
+    public void print() {
+        System.out.println("Why");
+    }
+
+    public static void main(String[] args) {
+        PrintWhy printWhy = new PrintWhy();
+        printWhy.print();
+    }
+}
---
+++ src/main/java/com/example/javaproject3/week1/PrintfEx.java
@@ -0,0 +1,13 @@
+package com.example.javaproject3.week1;
+
+public class PrintfEx {
+    public void print() {
+        System.out.printf("%s", "Hello\n");
+        System.out.println("Bye");
+    }
+
+    public static void main(String[] args) {
+        PrintfEx printfEx = new PrintfEx();
+        printfEx.print();
+    }
+}
