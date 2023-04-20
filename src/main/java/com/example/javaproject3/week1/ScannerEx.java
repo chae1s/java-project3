9a4e26676f649b6fbb3107b1e08220d2568b3d2e
chae1s <chae1zd.b@gmail.com>
2023-04-20 10:38:45 +0900

    fix: Scanner 안내문 출력  - ScannerEx

--- src/main/java/com/example/javaproject3/week1/ScannerEx.java
+++ src/main/java/com/example/javaproject3/week1/ScannerEx.java
@@ -6,6 +6,10 @@ public class ScannerEx {
 
     public void readTwoNumbersAndPlus() {
         Scanner sc = new Scanner(System.in);
-        System.out.println(sc.nextInt() + sc.nextInt());
+        System.out.println("두 개의 숫자를 입력하세요. 입력하고 엔터 입력하고 엔터");
+        System.out.println("첫 번째 숫자:");
+        System.out.println("첫 번째 숫자는 " + sc.nextInt() + "입니다.");
+        System.out.println("두 번째 숫자:");
+        System.out.println("두 번째 숫자는 " + sc.nextInt() + "입니다.");
     }
 }
