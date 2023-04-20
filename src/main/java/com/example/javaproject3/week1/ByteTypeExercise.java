a2cfed7211d0408a1cea53069ab5c92e616b6dea
chae1s <chae1zd.b@gmail.com>
2023-04-20 14:38:14 +0900

    feat: type 실습 byte, int  - ByteTypeExercise  - ScannerFormatting   -- int값을 입력받아 합 출력

---
+++ src/main/java/com/example/javaproject3/week1/ByteTypeExercise.java
@@ -0,0 +1,12 @@
+package com.example.javaproject3.week1;
+
+public class ByteTypeExercise {
+    public static void main(String[] args) {
+        byte byte1 = 10;
+        byte byte2 = 127;
+
+        System.out.println(byte1);
+
+
+    }
+}
