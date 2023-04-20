a9ce0fe9a0fcf67bccad71c67ce3846b82aa403b
chae1s <chae1zd.b@gmail.com>
2023-04-20 13:46:39 +0900

    feat: CodeUp1011~1013  - Codeup1011 문제풀이  - Codeup1012 문제풀이  - Codeup1013 문제풀이

---
+++ src/main/java/com/example/javaproject3/codeup/Codeup1011.java
@@ -0,0 +1,12 @@
+package com.example.javaproject3.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1011 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char c = sc.next().charAt(0);
+        System.out.printf("%c", c);
+
+    }
+}
