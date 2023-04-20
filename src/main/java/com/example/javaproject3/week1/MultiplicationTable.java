ce2ad4566a9fa9b4fd0a30916a4f1cdf65cf7d42
chae1s <chae1zd.b@gmail.com>
2023-04-20 11:30:33 +0900

    feat: printf 실습  - MultiplicationTable 추가  - PrintTwoValuesShortage 추가

---
+++ src/main/java/com/example/javaproject3/week1/MultiplicationTable.java
@@ -0,0 +1,11 @@
+package com.example.javaproject3.week1;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        //2 * 1 = 2
+        //2 * 2 = 4
+        System.out.println("2" + "*" + "2" + "=" + "4");
+        System.out.printf("%d * %d = %d", 2, 2, 4);
+        System.out.printf("%d * %d = %d", 2, 3, 6);
+    }
+}
---
+++ src/main/java/com/example/javaproject3/week1/PrintTwoValuesShortage.java
@@ -0,0 +1,7 @@
+package com.example.javaproject3.week1;
+
+public class PrintTwoValuesShortage {
+    public static void main(String[] args) {
+        System.out.printf("%s님 안녕하세요. %s", "안녕", "이손을봐 대박임");
+    }
+}
