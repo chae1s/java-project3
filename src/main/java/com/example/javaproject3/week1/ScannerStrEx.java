5888fdad6f53239920a600175f1eba9419449b73
chae1s <chae1zd.b@gmail.com>
2023-04-20 09:38:42 +0900

    feat: 문자열2개 입력 받아서 더하기  - ScannerStrEx  - ScannerStrExTest

---
+++ src/main/java/com/example/javaproject3/week1/ScannerStrEx.java
@@ -0,0 +1,11 @@
+package com.example.javaproject3.week1;
+
+import java.util.Scanner;
+
+public class ScannerStrEx {
+
+    public void inputStringsAndPlus() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.next() + sc.next());
+    }
+}
