865f57c63713a79a801e1524962b82292de21629
chae1s <chae1zd.b@gmail.com>
2023-04-20 11:49:59 +0900

    feat: printf 실습 날짜 출력하기  - PrintfDate   -- 숫자 앞에 0 붙이기

---
+++ src/main/java/com/example/javaproject3/week1/PrintfDate.java
@@ -0,0 +1,11 @@
+package com.example.javaproject3.week1;
+
+public class PrintfDate {
+    public static void main(String[] args) {
+        System.out.printf("%d\n", 4);
+        System.out.printf("%02d\n", 4);
+        System.out.printf("%02d\n", 11);
+        //날짜 출력하기
+        System.out.printf("%d-%02d-%02d", 2023, 4, 9);
+    }
+}
