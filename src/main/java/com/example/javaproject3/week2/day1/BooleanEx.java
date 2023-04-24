d47f05ee5e1432184ecf3715d961e8f2825219eb
chae1s <chae1zd.b@gmail.com>
2023-04-24 14:35:25 +0900

    feat: boolean 실습  - BooleanEx

---
+++ src/main/java/com/example/javaproject3/week2/day1/BooleanEx.java
@@ -0,0 +1,13 @@
+package com.example.javaproject3.week2.day1;
+
+public class BooleanEx {
+    public static void main(String[] args) {
+        boolean flag1 = false;
+        boolean flag2 = true;
+        boolean isPaymentSuccess = true;
+        boolean isBalanceSufficient = true;
+
+        System.out.println("isPaymentSuccess = " + isPaymentSuccess);
+        System.out.println("isBalanceSufficient = " + isBalanceSufficient);
+    }
+}
