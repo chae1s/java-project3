fda9bdd1587ab9c947198176eb38e0ecd025b4b7
chae1s <chae1zd.b@gmail.com>
2023-04-24 13:43:22 +0900

    feat: 형변환 실습  - NarrowingCasting

---
+++ src/main/java/com/example/javaproject3/week2/day1/NarrowingCasting.java
@@ -0,0 +1,12 @@
+package com.example.javaproject3.week2.day1;
+
+public class NarrowingCasting {
+    public static void main(String[] args) {
+        double dVal = 123.456;
+
+        int iVal = (int)dVal;
+
+        System.out.println("dVal = " + dVal);
+        System.out.println("iVal = " + iVal);
+    }
+}
