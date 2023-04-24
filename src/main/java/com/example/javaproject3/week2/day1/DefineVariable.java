8cac4974b18f4476680d704ba4ebe9518be7a1b1
chae1s <chae1zd.b@gmail.com>
2023-04-24 11:28:40 +0900

    feat: 변수 복습  - VariableEx  - DefineVariable

---
+++ src/main/java/com/example/javaproject3/week2/day1/DefineVariable.java
@@ -0,0 +1,18 @@
+package com.example.javaproject3.week2.day1;
+
+public class DefineVariable {
+
+    public static void main(String[] args) {
+        int num = 10;
+        String name = "Choonsik";
+        String name2 = new String("Choonsik");
+
+        String sOne = new String("1");
+        int iOne = 1;
+        float fOne = 1.0f;
+
+        System.out.println(iOne == fOne);
+
+    }
+
+}
