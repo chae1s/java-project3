8851e2e698d3577606ea864f2a3ffab0889b15e8
chae1s <chae1zd.b@gmail.com>
2023-04-26 14:03:46 +0900

    feat: 산술연산자 +, -, *, /, %  - ArithmeticOperator

---
+++ src/main/java/com/example/javaproject3/week2/day3/ArithmeticOperator.java
@@ -0,0 +1,13 @@
+package com.example.javaproject3.week2.day3;
+
+public class ArithmeticOperator {
+    public static void main(String[] args) {
+        int iVal1 = 3;
+        int iVal2 = 10;
+        System.out.printf("a + b = %d\n", iVal1 + iVal2);
+        System.out.printf("a - b = %d\n", iVal1 - iVal2);
+        System.out.printf("a * b = %d\n", iVal1 * iVal2);
+        System.out.printf("a / b = %d\n", iVal1 / iVal2);
+        System.out.printf("a %% b = %d\n", iVal1 % iVal2);
+    }
+}
