56d606d6ead9baf45367d545d5bf277d248f7c58
chae1s <chae1zd.b@gmail.com>
2023-04-26 10:33:24 +0900

    feat: 2차원 배열 실습  - ThreexThree  - TenxTen

---
+++ src/main/java/com/example/javaproject3/week2/day3/TenxTen.java
@@ -0,0 +1,19 @@
+package com.example.javaproject3.week2.day3;
+
+import java.util.Arrays;
+
+public class TenxTen {
+    public static void main(String[] args) {
+        int[][] arr = new int[10][10];
+        System.out.println(Arrays.toString(arr[0]));
+        System.out.println(Arrays.toString(arr[1]));
+        System.out.println(Arrays.toString(arr[2]));
+        System.out.println(Arrays.toString(arr[3]));
+        System.out.println(Arrays.toString(arr[4]));
+        System.out.println(Arrays.toString(arr[5]));
+        System.out.println(Arrays.toString(arr[6]));
+        System.out.println(Arrays.toString(arr[7]));
+        System.out.println(Arrays.toString(arr[8]));
+        System.out.println(Arrays.toString(arr[9]));
+    }
+}
