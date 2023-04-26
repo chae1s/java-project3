1d132397b7dd583156f615d87dd0e72d7ab48e69
chae1s <chae1zd.b@gmail.com>
2023-04-26 15:08:22 +0900

    feat: 산술연산자 실습 분, 초 구하기  - MinuteSecond

---
+++ src/main/java/com/example/javaproject3/week2/day3/MinuteSecond.java
@@ -0,0 +1,11 @@
+package com.example.javaproject3.week2.day3;
+
+public class MinuteSecond {
+    public static void main(String[] args) {
+        int seconds = 239;
+        int minute = seconds / 60;
+        int remainSeconds = seconds % 60;
+
+        System.out.printf("%d분 %s초", minute, remainSeconds);
+    }
+}
