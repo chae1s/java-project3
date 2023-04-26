187fc6566ecc6684751613c5eaa86fa6b0fd8358
chae1s <chae1zd.b@gmail.com>
2023-04-26 13:09:40 +0900

    feat: 2차원 배열 실습 row, column에 값 넣기  - TwoDimArrFillRow  - TwoDimArrFillColumn

---
+++ src/main/java/com/example/javaproject3/week2/day3/TwoDimArrFillRow.java
@@ -0,0 +1,21 @@
+package com.example.javaproject3.week2.day3;
+
+import java.util.Arrays;
+
+public class TwoDimArrFillRow {
+    public static void main(String[] args) {
+        int[][] arr = new int[5][5];
+
+        arr[2][0] = 1;
+        arr[2][1] = 2;
+        arr[2][2] = 3;
+        arr[2][3] = 4;
+        arr[2][4] = 5;
+
+        System.out.println(Arrays.toString(arr[0]));
+        System.out.println(Arrays.toString(arr[1]));
+        System.out.println(Arrays.toString(arr[2]));
+        System.out.println(Arrays.toString(arr[3]));
+        System.out.println(Arrays.toString(arr[4]));
+    }
+}
