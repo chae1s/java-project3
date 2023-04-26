f651c693fc3d0fc6aab7f44ed0176fed9e1c695c
chae1s <chae1zd.b@gmail.com>
2023-04-26 13:17:43 +0900

    fix: Row에 값 넣기 리팩토링  - TwoDimArrFillRow

--- src/main/java/com/example/javaproject3/week2/day3/TwoDimArrFillRow.java
+++ src/main/java/com/example/javaproject3/week2/day3/TwoDimArrFillRow.java
@@ -3,19 +3,25 @@ package com.example.javaproject3.week2.day3;
 import java.util.Arrays;
 
 public class TwoDimArrFillRow {
-    public static void main(String[] args) {
-        int[][] arr = new int[5][5];
+    public static void fillARow(int[][] arr, int rowNum) {
+        arr[rowNum][0] = 1;
+        arr[rowNum][1] = 2;
+        arr[rowNum][2] = 3;
+        arr[rowNum][3] = 4;
+        arr[rowNum][4] = 5;
+    }
 
-        arr[2][0] = 1;
-        arr[2][1] = 2;
-        arr[2][2] = 3;
-        arr[2][3] = 4;
-        arr[2][4] = 5;
-
+    public static void printArr(int[][] arr) {
         System.out.println(Arrays.toString(arr[0]));
         System.out.println(Arrays.toString(arr[1]));
         System.out.println(Arrays.toString(arr[2]));
         System.out.println(Arrays.toString(arr[3]));
         System.out.println(Arrays.toString(arr[4]));
+        System.out.println("----------------------");
+    }
+    public static void main(String[] args) {
+        int[][] arr = new int[5][5];
+        fillARow(arr, 2);
+        printArr(arr);
     }
 }
