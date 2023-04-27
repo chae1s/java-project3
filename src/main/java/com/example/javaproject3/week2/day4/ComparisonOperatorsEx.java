ba0c93a5902ee558d80725b650cfa7275a4a42da
chae1s <chae1zd.b@gmail.com>
2023-04-27 10:41:31 +0900

    feat: 비교연산자 실습  - ComparisonOperatorsEx  - ComparisonOperatorsEx2

---
+++ src/main/java/com/example/javaproject3/week2/day4/ComparisonOperatorsEx.java
@@ -0,0 +1,11 @@
+package com.example.javaproject3.week2.day4;
+
+public class ComparisonOperatorsEx {
+    public static void main(String[] args) {
+        int iVal1 = 10;
+        int iVal2 = 10;
+        boolean result = iVal1 == iVal2;
+
+        System.out.println("result = " + result);
+    }
+}
