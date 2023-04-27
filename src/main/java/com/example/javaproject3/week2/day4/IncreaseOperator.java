bd0d285aead0489900a2126b0aa69a7a6ead6f9c
chae1s <chae1zd.b@gmail.com>
2023-04-27 10:17:58 +0900

    feat: 증감연산자 실습  - IncreaseFirst  - IncreaseOperator

---
+++ src/main/java/com/example/javaproject3/week2/day4/IncreaseOperator.java
@@ -0,0 +1,19 @@
+package com.example.javaproject3.week2.day4;
+
+public class IncreaseOperator {
+    public static void main(String[] args) {
+        int i = 10;
+        System.out.println("i : " + i++);
+        System.out.println("i : " + i);
+
+        System.out.println("i : " + ++i);
+        System.out.println("i : " + i);
+
+        int j = 10;
+        System.out.println("j : " + j--);
+        System.out.println("j : " + j);
+
+        System.out.println("j : " + --j);
+        System.out.println("j : " + j);
+    }
+}
