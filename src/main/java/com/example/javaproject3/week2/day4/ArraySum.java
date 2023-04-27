2785ff7cf141188e30b11a83078597d2d4cca7a1
chae1s <chae1zd.b@gmail.com>
2023-04-27 09:24:33 +0900

    feat: 대입연산자 실습 배열 값 누적하기  - ArraySum

---
+++ src/main/java/com/example/javaproject3/week2/day4/ArraySum.java
@@ -0,0 +1,15 @@
+package com.example.javaproject3.week2.day4;
+
+public class ArraySum {
+    public static void main(String[] args) {
+        int answer = 0;
+        int[] arr = {2, 1, 7, 4};
+        answer = answer + arr[0];
+        answer = answer + arr[1];
+        answer = answer + arr[2];
+        answer = answer + arr[3];
+
+        System.out.println(answer);
+
+    }
+}
