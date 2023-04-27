1f29ff94dcf1b919cbcba63f8cbb86658587e243
chae1s <chae1zd.b@gmail.com>
2023-04-27 15:05:49 +0900

    feat: 비트 연산 실습  - BitOperatorEx  - BitOperatorEx2

---
+++ src/main/java/com/example/javaproject3/week2/day4/BitOperatorEx.java
@@ -0,0 +1,13 @@
+package com.example.javaproject3.week2.day4;
+
+public class BitOperatorEx {
+    public static void main(String[] args) {
+        int val1= 1;
+        System.out.println(val1 << 1);
+        System.out.println(val1 << 2);
+
+        int iVal2 = 32;
+        System.out.println(iVal2 >> 1);
+        System.out.println(iVal2 >> 2);
+    }
+}
---
+++ src/main/java/com/example/javaproject3/week2/day4/BitOperatorEx2.java
@@ -0,0 +1,9 @@
+package com.example.javaproject3.week2.day4;
+
+public class BitOperatorEx2 {
+    public static void main(String[] args) {
+        int val1 = 1;
+
+        System.out.println(Integer.toBinaryString(~1));
+    }
+}
