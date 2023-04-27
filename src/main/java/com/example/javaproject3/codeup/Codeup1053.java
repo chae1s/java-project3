0fde22cb3efe3f19ab123590851c2ac48f994f00
chae1s <chae1zd.b@gmail.com>
2023-04-27 13:20:11 +0900

    fix: CodeUp1053-1055  - Codeup1053  - Codeup1054  - Codeup1055

---
+++ src/main/java/com/example/javaproject3/codeup/Codeup1053.java
@@ -0,0 +1,11 @@
+package com.example.javaproject3.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1053 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int iVal = sc.nextInt();
+        System.out.println(iVal == 1 ? 0 : 1);
+    }
+}
