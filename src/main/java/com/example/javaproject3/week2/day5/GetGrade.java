7a7985283574e3ccbb819917076ae5035933760a
chae1s <chae1zd.b@gmail.com>
2023-04-28 10:31:58 +0900

    feat: 조건문 실습 else-if  - IfWrong  - GetGrade

---
+++ src/main/java/com/example/javaproject3/week2/day5/GetGrade.java
@@ -0,0 +1,28 @@
+package com.example.javaproject3.week2.day5;
+
+public class GetGrade {
+    public static void main(String[] args) {
+        int score = 75;
+
+        if (score >= 90) {
+            System.out.println("A");
+        } else if (score >= 80) {
+            System.out.println("B");
+        } else if (score >= 70) {
+            System.out.println("C");
+        } else {
+            System.out.println("F");
+        }
+
+        System.out.println("잘못된 실행 순서");
+        if (score >= 70) {
+            System.out.println("C");
+        } else if (score >= 80) {
+            System.out.println("B");
+        } else if (score >= 90) {
+            System.out.println("A");
+        } else {
+            System.out.println("F");
+        }
+    }
+}
