4446cbc999d59104ae2ae4991c8949449015e96c
chae1s <chae1zd.b@gmail.com>
2023-05-01 13:49:03 +0900

    feat: 반복문 실습 가-힣 출력  - LoopHangeul

---
+++ src/main/java/com/example/javaproject3/week3/day1/LoopHangeul.java
@@ -0,0 +1,9 @@
+package com.example.javaproject3.week3.day1;
+
+public class LoopHangeul {
+    public static void main(String[] args) {
+        for (int c = '가'; c <= '힣' ; c++) {
+            System.out.printf("%c\n", c);
+        }
+    }
+}
