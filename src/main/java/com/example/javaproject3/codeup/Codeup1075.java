137796b4cfcb3a5bdcf492412511ac809b8a5aee
chae1s <chae1zd.b@gmail.com>
2023-05-01 11:45:07 +0900

    feat: CodeUp1073-1075  - Codeup1073  - Codeup1074  - Codeup1075

---
+++ src/main/java/com/example/javaproject3/codeup/Codeup1075.java
@@ -0,0 +1,13 @@
+package com.example.javaproject3.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1075 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int iVal = sc.nextInt() - 1;
+        for (int i = iVal; i >= 0 ; i--) {
+            System.out.println(i);
+        }
+    }
+}
