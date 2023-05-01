b62f0c832279d4e149357c3a609341709b40cf8b
chae1s <chae1zd.b@gmail.com>
2023-05-01 11:28:03 +0900

    feat: 반복문 실습 역수 출력  - ForReverseWrong  - ForReverse

---
+++ src/main/java/com/example/javaproject3/week3/day1/ForReverse.java
@@ -0,0 +1,13 @@
+package com.example.javaproject3.week3.day1;
+
+public class ForReverse {
+    public static void main(String[] args) {
+        for (int i = 10; i > 0 ; i--) {
+            System.out.println(i);
+        }
+
+        for (int i = 10; i >= 0 ; i--) {
+            System.out.printf("%d ", i);
+        }
+    }
+}
---
+++ src/main/java/com/example/javaproject3/week3/day1/ForReverseWrong.java
@@ -0,0 +1,9 @@
+package com.example.javaproject3.week3.day1;
+
+public class ForReverseWrong {
+    public static void main(String[] args) {
+        for (int i = 10; i > 0 ; i++) {
+            System.out.println(i);
+        }
+    }
+}
