8239d141168702c4fd0c6a41b7f1db44c35d15a3
chae1s <chae1zd.b@gmail.com>
2023-05-03 10:12:52 +0900

    feat: CodeUp1071,1073 do-while 사용  - Codeup10711073_2

---
+++ src/main/java/com/example/javaproject3/codeup/num1000/Codeup10711073_2.java
@@ -0,0 +1,16 @@
+package com.example.javaproject3.codeup.num1000;
+
+import java.util.Scanner;
+
+public class Codeup10711073_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num;
+        do {
+            num = sc.nextInt();
+            if (num != 0) {
+                System.out.println(num);
+            }
+        } while (num != 0);
+    }
+}
