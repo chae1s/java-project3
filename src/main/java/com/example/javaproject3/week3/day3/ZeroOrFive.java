5c2e3f588ae57e741e3007b86589e291d89d104e
chae1s <chae1zd.b@gmail.com>
2023-05-03 09:22:53 +0900

    feat: 반복문 실습 5또는0으로 구성된 숫자  - ZeroOrFive

---
+++ src/main/java/com/example/javaproject3/week3/day3/ZeroOrFive.java
@@ -0,0 +1,24 @@
+package com.example.javaproject3.week3.day3;
+
+public class ZeroOrFive {
+    public static void main(String[] args) {
+        int n = 555;
+
+        isZeroOrFive(n);
+    }
+
+    public static void isZeroOrFive(int n) {
+
+        String flagTest = "0 또는 5로 이루어진 숫자";
+        while (n > 0) {
+            int remainder = n % 10;
+            if (remainder % 5 != 0) {
+                flagTest = "0 또는 5로 이루어진 숫자 아님";
+            }
+            n /= 10;
+
+        }
+
+        System.out.printf("%s\n", flagTest);
+    }
+}
