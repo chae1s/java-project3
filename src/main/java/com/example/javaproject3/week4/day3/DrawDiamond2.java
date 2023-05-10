2252a6fa027dbc71a0c7bc64a3a4f20f5a254873
chae1s <chae1zd.b@gmail.com>
2023-05-10 10:33:01 +0900

    fix: 알고리즘 다이아몬드 file출력 구현  - DrawDiamond2

--- src/main/java/com/example/javaproject3/week4/day3/DrawDiamond2.java
+++ src/main/java/com/example/javaproject3/week4/day3/DrawDiamond2.java
@@ -1,5 +1,8 @@
 package com.example.javaproject3.week4.day3;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class DrawDiamond2 {
@@ -26,12 +29,31 @@ public class DrawDiamond2 {
 
     }
 
-    public static void main(String[] args) throws IOException {
-        int h = 7;
-        int pivot = h / 2;
+    public static void printFileOutput(String filePath, int h) throws IOException {
+        File file = new File(filePath);
+        if (!file.exists()) {
+            file.createNewFile();
+        }
 
+        BufferedWriter bw = new BufferedWriter(new FileWriter(file, true));
+        for (int i = 0; i < h; i++) {
+            bw.write(makeALine(h, i));
+        }
+        bw.flush();
+        bw.close();
+    }
+
+    public static void printShape(int h) {
         for (int i = 0; i < h; i++) {
             System.out.print(makeALine(h, i));
         }
+    }
+
+    public static void main(String[] args) throws IOException {
+        int h = 7;
+        int pivot = h / 2;
+        String filePath = "C:/Users/user/Desktop/project/workspace/java-project3/diamond.txt";
+        printShape(h);
+        printFileOutput(filePath, h);
     }
 }
