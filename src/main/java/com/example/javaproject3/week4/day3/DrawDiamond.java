0771b2b1ce65d244f2b03a08d51358a8867aa1c1
chae1s <chae1zd.b@gmail.com>
2023-05-10 09:48:45 +0900

    fix: 알고리즘 다이아몬드 repeat 사용 안한 풀이 추가  - DrawDiamond

--- src/main/java/com/example/javaproject3/week4/day3/DrawDiamond.java
+++ src/main/java/com/example/javaproject3/week4/day3/DrawDiamond.java
@@ -5,12 +5,21 @@ public class DrawDiamond {
     public static String getRepeatedSymbol(String symbol, int n) {
         return symbol.repeat(n);
     }
+
+    public static String getRepeatedSymbolFor(String symbol, int n) {
+        String result = "";
+        for (int i = 0; i < n; i++) {
+            result += symbol;
+        }
+
+        return result;
+    }
     public static void main(String[] args) {
         int h = 7;
         int pivot = h / 2;
         for (int i = 0; i < h; i++) {
             if (i <= pivot) {
-                System.out.printf("%s%s\n", getRepeatedSymbol("0", pivot - i), getRepeatedSymbol("*", 2 * i + 1));
+                System.out.printf("%s%s\n", getRepeatedSymbolFor("0", pivot - i), getRepeatedSymbolFor("*", 2 * i + 1));
             } else {
                 System.out.printf("%s%s\n", getRepeatedSymbol("0", i - pivot), getRepeatedSymbol("*", 2 * (h - i) - 1));
             }
