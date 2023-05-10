3101f21eae835970abe4bf1909d437a988d649fd
chae1s <chae1zd.b@gmail.com>
2023-05-10 15:08:00 +0900

    feat: 인터페이스 실습 Hello 출력  - HelloPrinter  - Printer2  - ConsoleMessagePrinterPrinter  - FileMessagePrinterPrinter

---
+++ src/main/java/com/example/javaproject3/week4/day3/ConsoleMessagePrinter.java
@@ -0,0 +1,8 @@
+package com.example.javaproject3.week4.day3;
+
+public class ConsoleMessagePrinter implements Printer2{
+    @Override
+    public void print(String message) {
+        System.out.println(message);
+    }
+}
